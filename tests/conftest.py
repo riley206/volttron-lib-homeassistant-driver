a4412d220558612c062da654bb9bae10bf9ae6d1
riley206 <rileymaltos@gmail.com>
2024-10-28 09:50:23 -0700

    added conftest

---
+++ tests/conftest.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*- {{{
+# ===----------------------------------------------------------------------===
+#
+#                 Installable Component of Eclipse VOLTTRON
+#
+# ===----------------------------------------------------------------------===
+#
+# Copyright 2022 Battelle Memorial Institute
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy
+# of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# ===----------------------------------------------------------------------===
+# }}}
+import sys
+from pathlib import Path
+
+path = Path(__file__).parent.parent.joinpath('src')
+
+if path.resolve() not in sys.path:
+    sys.path.insert(0, path.resolve().as_posix())
+
+from volttrontesting.fixtures import get_pyproject_toml
+from volttrontesting.fixtures.volttron_platform_fixtures import volttron_instance
